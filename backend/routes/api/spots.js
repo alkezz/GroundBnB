47bf930e3c7c201872dc6b373c31ea1e50042f94
Ali <a.k.ezzeddine@gmail.com>
2022-09-23 19:03:33 -0400

    Routes

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const router = express.Router();
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
-const { User, Spot } = require('../../db/models');
+const { User, Spot, Review, Booking, ReviewImage, SpotImage } = require('../../db/models');
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 
 router.get('/', async (req, res) => {
-    const allSpots = Spot.findAll()
+    const allSpots = await Spot.findAll({
+        include: {
+            model: Review
+        }
+    })
     res.status(200).json(allSpots)
 })
 
