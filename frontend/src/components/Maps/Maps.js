19778e8a82bc76ac39caccd5fd8f0a7f23248d9e
Ali <a.k.ezzeddine@gmail.com>
2023-03-09 16:59:58 -0500

    added pop-up to map. clicking marker on map will open div showing more info about that spot

--- frontend/src/components/Maps/Maps.js
+++ frontend/src/components/Maps/Maps.js
@@ -1,14 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Map, Marker, GoogleApiWrapper } from 'google-maps-react';
+import { Link, NavLink } from 'react-router-dom';
+import { Map, Marker, GoogleApiWrapper, InfoWindow } from 'google-maps-react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Modal from '@mui/material/Modal'
 let key
 const Maps = ({ allSpots }) => {
+    const [activeMarker, setActiveMarker] = useState(null);
+    const [selectedLocation, setSelectedLocation] = useState(null);
+    const [image, setImage] = useState(null)
+    const [open, setOpen] = useState(false)
+    const onMarkerClick = (marker, location) => {
+        setActiveMarker(marker);
+        setSelectedLocation(location);
+    };
+
+    const onInfoWindowClose = () => {
+        setActiveMarker(null);
+        setSelectedLocation(null);
+    };
     key = useSelector((state) => state.maps.key);
     return (
         <Map zoom={12.5} containerStyle={{ width: "25%", marginRight: "-655px", height: "100%" }} style={{ width: "663px", height: "700px" }} initialCenter={{ lat: 40.6991, lng: -73.9712 }} google={window.google}>
             {allSpots.map((item, index) => (
-                <Marker key={index} position={{ lat: item.lat, lng: item.lng }} title={item.name} />
+                <Marker onClick={(e, marker) => onMarkerClick(marker, item)} key={index} position={{ lat: item.lat, lng: item.lng }} title={item.name} />
             ))}
+            {activeMarker && selectedLocation && (
+                <InfoWindow
+                    marker={activeMarker}
+                    visible={activeMarker}
+                    onClose={onInfoWindowClose}
+                >
+                    <div style={{ width: "350px", height: "450px" }}>
+                        <img style={{ width: "100%", height: "75%" }} src={selectedLocation.previewImage} />
+                        <a style={{ textDecoration: "none", color: "black", width: "fit-content" }} href={`/spots/${selectedLocation.id}`}><h2>{selectedLocation.name}</h2></a>
+                        <h4>{selectedLocation.description}</h4>
+                    </div>
+                </InfoWindow>
+            )}
+            <Modal open={open} onClose={(e) => setOpen(false)}>
+                <img src={image} />
+            </Modal>
         </Map>
     );
 };
