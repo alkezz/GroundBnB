b70917377665fa32809001c2e8cc7492919508f2
Ali <a.k.ezzeddine@gmail.com>
2023-03-10 13:13:14 -0500

    reworking one spot page to look better than current style

--- frontend/src/components/OneSpot/index.js
+++ frontend/src/components/OneSpot/index.js
@@ -7,6 +7,8 @@ import { useDispatch, useSelector } from 'react-redux';
 import { useHistory, useParams } from 'react-router-dom';
 import { csrfFetch } from '../../store/csrf';
 import EditCommentModal from '../EditComment/EditCommentModal';
+import { Modal } from '../../context/Modal';
+import LoginForm from '../LoginFormModal/LoginForm'
 import EditSpotModal from '../EditSpotPage/EditSpotModal';
 import Calendar from 'react-calendar';
 import "./Calendar.css"
@@ -47,6 +49,7 @@ function SpotById() {
     const [errors, setErrors] = useState([])
     const [update, setUpdate] = useState(false)
     const [bookings, setBookings] = useState([])
+    const [showLogInModal, setShowLogInModal] = useState(false);
     useEffect(() => {
         dispatch(sessionActions.restoreUser())
     }, [dispatch])
@@ -136,7 +139,6 @@ function SpotById() {
         await dispatch(reviewActions.deleteReview(review))
     }
     if (!spots) return null
-    console.log("SPOTS", spots)
     return (
         <>
             {spots.SpotImages && (
@@ -167,57 +169,14 @@ function SpotById() {
                     </div>
                     <div id='img-div'>
                         <div className='first-image-upload-placeholder'>
-                            {spots?.SpotImages[0]?.url && (
-                                <>
-                                    <img style={{ height: "500px", width: "400px" }} src={spots.SpotImages[0].url} alt="cave"></img>
-                                </>
-                            )}
-                            {!spots?.SpotImages[0]?.url && (
-                                <img src={"https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/681px-Placeholder_view_vector.svg.png"}></img>
-                            )}
+                            <img style={{ height: "500px", width: "400px" }} src={spots.SpotImages[0].url} alt="cave"></img>
                         </div>
                         <div className='first-image-upload-placeholder'>
-                            {spots?.SpotImages[1]?.url && (
-                                <>
-                                    <img style={{ height: "500px", width: "350px" }} src={spots.SpotImages[1].url}></img>
-                                    {/* <label htmlFor='file-input' onChange={(e) => handleImageUpload(e, firstPictureId)}>
-                                        <div>
-                                            {!spots?.SpotImages[1]?.url && (
-                                                <i class="fa-solid fa-circle-plus"></i>
-                                            )}
-                                            {spots?.SpotImages[1]?.url && (
-                                                <img style={{ height: "500px", width: "350px" }} src={spots.SpotImages[1].url}></img>
-                                            )}
-                                        </div>
-                                    </label>
-                                    <input onChange={(e) => firstPictureId !== undefined ? handleImageUpload(e, firstPictureId) : handleImageUpload(e)} style={{ visibility: "hidden" }} id='file-input' type='file' name='file' encType="multipart/form-data" /> */}
-                                </>
-                            )}
-                            {!spots?.SpotImages[1]?.url && (
-                                <img src={"https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/681px-Placeholder_view_vector.svg.png"}></img>
-                            )}
+                            <img style={{ height: "500px", width: "350px" }} src={spots.SpotImages[1].url}></img>
                         </div>
                         <div className='second-image-upload-placeholder'>
                             <div className='second-image-upload-container'>
-                                {spots?.SpotImages[2]?.url && (
-                                    <>
-                                        <img style={{ height: "250px", width: "253px" }} src={spots.SpotImages[2].url}></img>
-                                        {/* <label htmlFor='file-input-2' onChange={(e) => handleImageUploadTwo(e, spots?.SpotImages[2].id)}>
-                                            <div>
-                                                {!spots?.SpotImages[2]?.url && (
-                                                    <i class="fa-solid fa-circle-plus"></i>
-                                                )}
-                                                {spots?.SpotImages[2]?.url && (
-                                                    <img style={{ height: "250px", width: "253px" }} src={spots.SpotImages[2].url}></img>
-                                                )}
-                                            </div>
-                                        </label>
-                                        <input onChange={(e) => spots.SpotImages[2].url !== undefined ? handleImageUploadTwo(e, secondPictureId) : handleImageUploadTwo(e)} style={{ visibility: "hidden" }} id='file-input-2' type='file' name='file' encType="multipart/form-data" /> */}
-                                    </>
-                                )}
-                                {!spots?.SpotImages[2]?.url && (
-                                    <img src={"https://upload.wikimedia.org/wikipedia/commons/thumb/3/3f/Placeholder_view_vector.svg/681px-Placeholder_view_vector.svg.png"}></img>
-                                )}
+                                <img style={{ height: "250px", width: "253px" }} src={spots.SpotImages[2].url}></img>
                             </div>
                             <div className='second-image-upload-container'>
                                 <img style={{ height: "250px", width: "253px" }} src={spots?.SpotImages[3]?.url}></img>
@@ -281,12 +240,6 @@ function SpotById() {
                         <li key='wifi'>
                             <i style={{ width: "25px" }} class="fa-solid fa-wifi"></i> WiFi
                         </li>
-                        {/* <br /> */}
-                        {/* <li key='wifi' style={{ width: "fit-content" }}>
-                            <i style={{ width: "25px" }} class="fa-solid fa-medal"></i> {spots?.Owner?.firstName} is a Superhost
-                            <br />
-                            <span>Superhosts are experienced, highly rated hosts who are committed to providing great stays for guests.</span>
-                        </li> */}
                     </ul>
                 </div>
                 <div style={{ borderBottom: '1px #dddddd solid', display: 'flex', marginLeft: '20%', marginRight: '40%' }}></div>
@@ -308,8 +261,8 @@ function SpotById() {
             <br />
             <div style={{ borderBottom: '1px #dddddd solid', display: 'flex', marginLeft: '20%', marginRight: '40%' }}></div>
             &nbsp;
-            {user?.id !== spots?.ownerId && (
-                <div style={{ visibility: !user || user.id === spots.ownerId ? "hidden" : "visible", display: "flex", flexDirection: "column", alignItems: "center", border: "1px solid #dddddd", paddingBottom: "50px", borderRadius: "15px", backgroundColor: "#ffffff", width: "400px", height: "400px", marginBottom: "10px", marginLeft: "62%", bottom: "-1000px", position: "sticky", top: "-1000px", boxShadow: "0 12px 24px rgba(0, 0, 0, 0.2)" }}>
+            {user?.id !== spots?.ownerId && user && (
+                <div className='book-spot-card'>
                     <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", width: "85%" }}>
                         <h2 style={{ marginBottom: "10px" }}>${spots.price} <span style={{ fontSize: "16px" }}>night</span></h2>
                         <div style={{ marginTop: "25px", fontWeight: "600", fontSize: "14px" }}>
@@ -402,8 +355,104 @@ function SpotById() {
                     )}
                 </div>
             )}
+            {!user && (
+                <div className='book-spot-card'>
+                    <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", width: "85%" }}>
+                        <h2 style={{ marginBottom: "10px" }}>${spots.price} <span style={{ fontSize: "16px" }}>night</span></h2>
+                        <div style={{ marginTop: "25px", fontWeight: "600", fontSize: "14px" }}>
+                            <span key={spots.id} style={{ visibility: isNaN(spots.avgStarRating) || !user ? "hidden" : "visible" }}> <i class="fa-solid fa-star"></i> </span>
+                            <span style={{ marginTop: "5px" }} key={spots.id + 1}> {isNaN(spots.avgStarRating) ? "No Reviews Yet!" : avgRating} </span>
+                            &nbsp;
+                            ·
+                            &nbsp;
+                            <span>{reviews.length} reviews</span>
+                        </div>
+                    </div>
+                    <div style={{ display: "flex" }}>
+                        <label style={{ border: "1px solid black", height: "55px", borderTopLeftRadius: "15px", borderBottomLeftRadius: "15px" }}>
+                            <span style={{ fontWeight: "700", fontSize: "14px", marginLeft: "35px" }}>CHECK-IN</span>
+                            <br />
+                            &nbsp;
+                            &nbsp;
+                            &nbsp;
+                            &nbsp;
+                            <input value={startDate} style={{ border: "none", marginRight: "10px" }} type="date" onChange={(e) => setStartDate(e.target.value)} />
+                        </label>
+                        <label style={{ borderTop: "1px solid black", borderRight: "1px solid black", borderBottom: "1px solid black", borderTopRightRadius: "15px", borderBottomRightRadius: "15px" }}>
+                            <span style={{ fontWeight: "700", fontSize: "14px", marginLeft: "20px" }}>&nbsp;&nbsp;CHECKOUT</span>
+                            <br />
+                            &nbsp;
+                            &nbsp;
+                            &nbsp;
+                            <input value={endDate} style={{ border: "none", marginRight: "10px" }} type="date" onChange={(e) => setEndDate(e.target.value)} />
+                        </label>
+                    </div>
+                    <br />
+                    <br />
+                    {startDate && endDate && startDate > endDate && (
+                        <>
+                            <i style={{ color: "red" }} className="fa-solid fa-circle-xmark fa-3x"></i>
+                            <h3 style={{ color: "red", marginLeft: "15px" }}>Please make sure your start date comes before your end date...</h3>
+                        </>
+                    )}
+                    {startDate && endDate && startDate === endDate && (
+                        <>
+                            <i style={{ color: "red" }} className="fa-solid fa-circle-xmark fa-3x"></i>
+                            <h3 style={{ color: "red", marginLeft: "15px" }}>Please make sure your start date and end date differ...</h3>
+                        </>
+                    )}
+                    {startDate && endDate && startDate < todaysDate.toISOString().substring(0, 10) && (
+                        <>
+                            <i style={{ color: "red" }} className="fa-solid fa-circle-xmark fa-3x"></i>
+                            <h3 style={{ color: "red", marginLeft: "15px" }}>Please make sure your start date does not come before todays date...</h3>
+                        </>
+                    )}
+                    {startDate && endDate && startDate < endDate && startDate >= todaysDate.toISOString().substring(0, 10) && (
+                        <div>
+                            {!hasBooking && (
+                                <>
+                                    <button onClick={(e) => setShowLogInModal(true)} style={{ backgroundColor: "#d60565", color: "white", borderRadius: "10px", cursor: "pointer", width: "100%", height: "50px", fontWeight: "700", fontSize: "16px", border: "none" }}>Reserve</button>
+                                    {showLogInModal && (
+                                        <Modal onClose={() => setShowLogInModal(false)}>
+                                            <LoginForm />
+                                        </Modal>
+                                    )}
+                                </>
+
+                            )}
+                            &nbsp;
+                            <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
+                                <span style={{ textDecoration: "underline" }}>
+                                    ${spots.price} x {Math.floor((Date.parse(endDate) - Date.parse(startDate)) / 86400000)} nights
+                                </span>
+                                <span>${spots.price * Math.floor((Date.parse(endDate) - Date.parse(startDate)) / 86400000)}</span>
+                            </div>
+                            <br />
+                            <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
+                                <span style={{ textDecoration: "underline" }}>Cleaning fee</span>
+                                <span>$50</span>
+                            </div>
+                            <br />
+                            <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", width: "350px" }}>
+                                <span style={{ textDecoration: "underline" }}>Service fee</span>
+                                <span>$50</span>
+                            </div>
+                            <br />
+                            <div style={{ borderBottom: "1px solid #dddddd" }} />
+                            <br />
+                            <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between" }}>
+                                <span style={{ fontWeight: "700" }}>Total before taxes</span>
+                                <span style={{ fontWeight: "700" }}>${spots.price * Math.floor((Date.parse(endDate) - Date.parse(startDate)) / 86400000) + 100}</span>
+                            </div>
+                            <br />
+                            <br />
+                            <br />
+                        </div>
+                    )}
+                </div>
+            )}
             {user?.id === spots?.ownerId && (
-                <div style={{ display: "flex", flexDirection: "column", alignItems: "center", border: "1px solid black", paddingBottom: "50px", borderRadius: "15px", backgroundColor: "#ffffff", width: "400px", height: "400px", marginBottom: "10px", marginLeft: "62%", bottom: "550px", position: "sticky", top: "110px", marginTop: "-220px", boxShadow: "2px 2px 2px black" }}>
+                <div className='book-spot-card'>
                     <div style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", width: "85%" }}>
                         <h2 style={{ marginBottom: "10px" }}>${spots.price} <span style={{ fontSize: "16px" }}>night</span></h2>
                         <div style={{ marginTop: "25px", fontWeight: "600", fontSize: "14px" }}>
